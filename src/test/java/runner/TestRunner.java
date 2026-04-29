c171e5c5015494f37b971372be423e672333535e
Namitha D Souza <namitrhadsouza99@gmail.com>
2026-04-29 19:32:14 +0530

    adhoc fixes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,6 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepdefs", "config"},
+        //tags = "@namitha",
         plugin = {"pretty", "html:target/cucumber-reports"}
 )
 public class TestRunner {
